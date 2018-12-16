832eb015e4ce9136b85de6f585d05a518a3640a1
Jack <lijaack@yahoo.com>
2018-12-16 01:55:30 -0800

    added prompts

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,17 +19,69 @@ connection.connect(function(err) {
   displayItems();
 });
 
+var itemLength;
+
 function displayItems(){
+
     connection.query("SELECT * FROM products", function(err, res) {
+
         if (err) throw err;
         var table = new cliTable({
+
             head:["item id", "product name", "department name","price", "stock quanity"]
-          })
+            
+        })
 
         for(var i = 0; i < res.length; i++){
-        table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
-        )
+            
+            table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity])
+            
         }    
+
         console.log(table.toString())  
+
+        itemLength = res.length;
+
+        promptCustomer();
+
     });
+
 };
+
+function promptCustomer(){
+
+    inquirer.prompt([
+
+        {
+            type: 'input',
+            name: 'item',
+            message: 'Enter the item id of the item you would like to purchase'
+        },
+        {
+            type: 'input',
+            name: 'quantity',
+            message: 'How many would you like to purchase?'
+        }
+
+    ]).then(function(response){
+
+        var itemNum = response.item;
+        var quanity = response.quanity;
+
+        console.log(itemLength);
+
+        if (itemNum < itemLength && itemNum > 0){
+
+            connection.query("SELECT * FROM products", function(err, res) {
+
+            });
+
+        } else {
+
+            console.log("item not found, select a different item");
+            promptCustomer();
+
+        };
+    });
+
+}
