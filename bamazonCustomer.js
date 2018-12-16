092c466b9816b7df1a2ca36991cea52d9b84de92
Jack <lijaack@yahoo.com>
2018-12-16 00:55:34 -0800

    displaying table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var cliTable = require("cli-table");
+
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -20,8 +22,14 @@ connection.connect(function(err) {
 function displayItems(){
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
-        console.log("hello");
+        var table = new cliTable({
+            head:["item id", "product name", "department name","price", "stock quanity"]
+          })
 
-        console.log(res);
+        for(var i = 0; i < res.length; i++){
+        table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+        )
+        }    
+        console.log(table.toString())  
     });
 };
