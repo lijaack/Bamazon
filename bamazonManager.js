a9788ce9e9cf4e604fccea2dc7194dfda612ee7e
Jack <lijaack@yahoo.com>
2018-12-17 23:05:08 -0800

    manager add inventory function is fully functional

--- bamazonManager.js
+++ bamazonManager.js
@@ -19,6 +19,7 @@ connection.connect(function(err) {
   promptManager()
 });
 
+// ==========================================================================ask the manager what he wants to do ===========================================================================================
 
 function promptManager(){
   inquirer.prompt([
@@ -49,6 +50,9 @@ function promptManager(){
   });
 }
 
+
+
+//========================================================================== display items ===========================================================================================
 
 function displayItems(){
 
@@ -89,8 +93,11 @@ function displayItems(){
 
 };
 
+//========================================================================== display low inventory===========================================================================================
 function displayLow(){
   console.log("hello")
+
+  //selects all from database and displays a table
   connection.query("SELECT * FROM products WHERE stock_quantity < 20", function(err, res) {
 
     if (err) throw err;
@@ -109,6 +116,7 @@ function displayLow(){
 
     console.log(table.toString())  
 
+//ask if the manager wants to do anything else
     inquirer.prompt([
       {
           type: "confirm",
@@ -132,12 +140,153 @@ function displayLow(){
 
 
 
-
+//========================================================================== add inventory===========================================================================================
 
 function addInventory(){
 
+  var itemLength;
+//selects all from database and displays a table
+  connection.query("SELECT * FROM products", function(err, res) {
+
+    if (err) throw err;
+    itemLength = res.length;
+
+    var table = new cliTable({
+
+        head:["item id", "product name", "department name","price", "stock quantity"]
+
+    })
+
+    for(var i = 0; i < res.length; i++){
+        
+        table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity])
+        
+    }    
+    console.log(table.toString())  
+
+// ask the manager which item he wants to select and how many items he would like to add
+
+    inquirer.prompt([
+      {
+        type:"input",
+        name:"itemNum",
+        message:"Which item would you like to add inventory to?"
+      },
+      {
+        type:"input",
+        name:"updateQuantity",
+        message:"How many would you like to add?"
+      }
+      ]).then(function(response){
+        var itemNum = parseInt(response.itemNum);
+        var updateQuantity = parseInt(response.updateQuantity);
+        var itemName;
+        var currentQuantity;
+        var updateQuantity;
+        
+        if (itemNum <= itemLength && itemNum > 0){
+
+// confirms the manager chose the right item and added the right quantity
+            connection.query("SELECT * FROM products", function(err, res) {
+
+                for (var i = 0; i < res.length; i++){
+
+                    if(itemNum === res[i].item_id){
+                        currentQuantity = res[i].stock_quantity;
+                        itemName = res[i].product_name;
+                    }
+                }
+                confirmUpdate()
+            });
+
+            function confirmUpdate(){
+                if(updateQuantity > 0){
+
+                    inquirer.prompt([
+                        {
+                            type: "confirm",
+                            name:"confirmUpdate",
+                            message: "You are adding " + updateQuantity + " inventory to " + itemName + ". Is this correct?"
+                        }
+                    ]).then(function(response){
+                        if (response.confirmUpdate){
+                            update();
+                        } else{
+                            promptManager();
+                        };
+                    });
+
+                } else {
+                    
+                    inquirer.prompt([
+                        {
+                            type: "input",
+                            name: "changeQuantity",
+                            message: "You have entered an invalid quantity, please change your quantity."
+                        }
+                    ]).then(function(response){
+                        updateQuantity = response.changeQuantity;
+                        confirmUpdate();
+                    });
+                };
+            };
+
+
+// if everything is confirmed, update the database
+            function update(){
+                newInventory = currentQuantity + updateQuantity;
+
+                connection.query(
+                    "UPDATE products SET ? WHERE ?",
+                    [
+                        {
+                            stock_quantity: newInventory
+                        },
+                        {
+                            item_id: itemNum
+                        }
+                    ],
+                    function(err, res) {
+
+                        if (err) throw err;
+                        
+                        inquirer.prompt([
+                          {
+                              type: "confirm",
+                              name: "doAgain",
+                              message: "Your inventory has been successfully updated. Would you like to do something else?",
+                    
+                          }
+                        ]).then(function(response){
+                          if (response.doAgain){
+                            promptManager();
+                          } else {
+                            connection.end();
+                          };
+                    
+                        });
+                });
+            }
+
+        } else {
+
+            console.log("item not found, select a different item");
+            addInventory();
+
+        };
+
+
+
+      });
+
+    });
+
 }
 
+//========================================================================== add new product===========================================================================================
 function addNewProduct(){
+
+
+
 
 }
