e35cc27838c191c7046a4ea0b6afab67fd117704
Jack <lijaack@yahoo.com>
2018-12-24 00:02:35 -0800

    added new departments table

--- bamazon.sql
+++ bamazon.sql
@@ -10,8 +10,18 @@ CREATE TABLE products(
     department_name VARCHAR(45) NOT NULL,
     price DECIMAL(10,2) NOT NULL,
     stock_quantity INT(100) NOT NULL,
+    product_sales INT(100) NOT NULL
     PRIMARY KEY (item_id)
 );
+
+CREATE TABLE departments(
+  department_id INT NOT NULL AUTO_INCREMENT,
+  department_name VARCHAR(100) NOT NULL,
+  over_head_costs  INT(100) NOT NULL,
+
+);
+
+
 
 INSERT INTO products(product_name, department_name, price, stock_quantity)
 
