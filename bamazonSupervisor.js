ab4c1507b67e974892487c48340fadaf001ea99d
Jack <lijaack@yahoo.com>
2018-12-24 23:54:51 -0800

    added prompts for supervisor

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -16,4 +16,36 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
+  promptSupervisor()
 });
+
+function promptSupervisor(){
+  inquirer.prompt([
+    {
+        type: "list",
+        name: "doWhat",
+        message: "What would you like to do?",
+        choices: ["View Products Sales by Department", "Create New Department", "exit console"]
+    }
+  ]).then(function(response){
+    
+    switch(response.doWhat){
+      case "View Products Sales by Department": viewSales();
+      break;
+
+      case "Create New Department": createNew();
+      break;
+
+      case "exit console": connection.end();
+      break;
+    }
+  });
+}
+
+function viewSales(){
+  
+};
+
+function createNew(){
+
+};
