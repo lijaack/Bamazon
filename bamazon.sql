1ec3756e80718d03f625c26aa11f0a727e86725b
Jack <lijaack@yahoo.com>
2018-12-25 22:50:40 -0800

    finished supervisor sql manager

--- bamazon.sql
+++ bamazon.sql
@@ -28,7 +28,10 @@ INSERT INTO products(product_name, department_name, price, stock_quantity)
 
 VALUES ("milk","grocery", 2.99, 50), ("eggs","grocery", 3.99, 100), ("doritos","snacks", 1.99, 70), ("cheetos","snacks", 1.99, 70), ("chicken breast","grocery", 9.99, 30), ("pork","grocery", 8.99, 10), 
 ("beef","grocery", 14.99, 30), ("candy","snacks", .99, 100), ("yogurt","dairy", 1.50, 40), ("orange juice","beverages", 2.99, 15), ("macbook","electronics", 1499.99, 50), ("ps4","electronics", 299.99, 50), 
-("xbox one","electronics", 299.99, 50)
+("xbox one","electronics", 299.99, 50);
 
 
 
+INSERT INTO departments(department_name, over_head_costs)
+
+VALUES ("grocery", 20000), ("snacks", 10000), ("dairy", 10000), ("beverages", 10000), ("electronics", 100000);
--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -43,9 +43,74 @@ function promptSupervisor(){
 }
 
 function viewSales(){
-  
+
+  connection.query("SELECT departments.department_id, departments.department_name, products.product_sales, departments.over_head_costs, COALESCE(sum(products.product_sales),0) AS total_sales, COALESCE(sum(products.product_sales),0) - departments.over_head_costs FROM products RIGHT JOIN departments ON products.department_name = departments.department_name GROUP BY departments.department_name", function(err, res) {
+    var table = new cliTable({
+        head:["department id", "department name", "overhead cost", "product sales", "total profit"]
+    });
+    for(var i = 0; i < res.length; i++){
+            
+      table.push([res[i].department_id, res[i].department_name, res[i].over_head_costs, res[i].total_sales, res[i].total_sales - res[i].over_head_costs])
+      
+    }    
+    console.log(table.toString())  
+    inquirer.prompt([
+      {
+          type: "confirm",
+          name: "doAgain",
+          message: "Here are the products sales by department, would you like to do something else?",
+
+      }
+    ]).then(function(response){
+      if (response.doAgain){
+        promptSupervisor();
+      } else {
+        connection.end();
+      };
+
+    });
+  });
+
+
 };
 
 function createNew(){
+  inquirer.prompt([
+    {
+      type:"input",
+      name:"departmentName",
+      message:"What department would you like to add?"
+    },
+    {
+      type:"input",
+      name:"overheadCost",
+      message:"What is the over head cost for this department?"
+    }
+    ]).then(function(response){
+        var departmentName = response.departmentName;
+        var overheadCost = response.overheadCost;
+  
 
+        connection.query(
+            "INSERT INTO departments (department_name, over_head_costs) VALUES ('" + departmentName + "', " + overheadCost + ")",              
+            function(err, res) {
+                if (err) throw err;
+
+                inquirer.prompt([
+                    {
+                        type: "confirm",
+                        name: "doAgain",
+                        message: "The department has been successfully added. Would you like to do something else?",
+              
+                    }
+                ]).then(function(response){
+                    if (response.doAgain){
+                    promptSupervisor();
+                    } else {
+                    connection.end();
+                    };
+            
+                });
+        });
+    });
 };
